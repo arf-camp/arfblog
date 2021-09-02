c76ae4f391b646a84a5aa765b5e76769cad77f3e
arf-camp <asheqrahman2621@gmail.com>
2021-09-03 04:34:27 +0600

    completing front end and signup in logout function

---
+++ backend/includes/footer.php
@@ -0,0 +1,23 @@
+                <!--start footer-->
+                <footer class="footer mt-auto footer-light">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-6 small">Copyright &#xA9; <a target="_blank" href="https://arfcamp.com/">ARF</a> <?php echo date('Y') ?></div>
+                            <div class="col-md-6 text-md-right small">
+                                <a href="#!">Privacy Policy</a>
+                                &#xB7;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+                <!--end footer-->
+            </div>
+        </div>
+
+        <!--Script JS-->
+        <script src="js/jquery-3.4.1.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
