3d1c11be041e7a385f96b3729d6371c60fe673ef
arf-camp <asheqrahman2621@gmail.com>
2021-09-23 04:15:43 +0600

    updating cooment section

--- backend/includes/footer.php
+++ backend/includes/footer.php
@@ -3,7 +3,7 @@
                 <footer class="footer mt-auto footer-light">
                     <div class="container-fluid">
                         <div class="row">
-                            <div class="col-md-6 small">Copyright &#xA9; <a target="_blank" href="https://arfcamp.com/">ARF</a> <?php echo date('Y') ?></div>
+                            <div class="col-md-6 small">Copyright &#xA9; <a target="_blank" href="https://www.instagram.com/asheq2621/">ARF</a> <?php echo date('Y') ?></div>
                             <div class="col-md-6 text-md-right small">
                                 <a href="#!">Privacy Policy</a>
                                 &#xB7;
